f2f59b0f0c9a162634728603db6879443ac8eb64
ZenoZone <enzo.silva76@etec.sp.gov.br>
2024-05-07 10:45:49 -0300

    comecando outra atividade

--- condicionalNiviaDois/Form2.Designer.cs
+++ condicionalNiviaDois/Form2.Designer.cs
@@ -28,7 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtvalor = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnwhile = new System.Windows.Forms.Button();
+            this.btndowhile = new System.Windows.Forms.Button();
+            this.btnfor = new System.Windows.Forms.Button();
+            this.btnlimpar = new System.Windows.Forms.Button();
+            this.btnvoltar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -39,15 +50,115 @@
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(146, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Digite um número";
+            // 
+            // txtvalor
+            // 
+            this.txtvalor.Location = new System.Drawing.Point(265, 72);
+            this.txtvalor.Name = "txtvalor";
+            this.txtvalor.Size = new System.Drawing.Size(100, 20);
+            this.txtvalor.TabIndex = 2;
+            this.txtvalor.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(149, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Resultado";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(265, 140);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 112);
+            this.textBox1.TabIndex = 5;
+            // 
+            // btnwhile
+            // 
+            this.btnwhile.Location = new System.Drawing.Point(149, 310);
+            this.btnwhile.Name = "btnwhile";
+            this.btnwhile.Size = new System.Drawing.Size(75, 23);
+            this.btnwhile.TabIndex = 6;
+            this.btnwhile.Text = "while";
+            this.btnwhile.UseVisualStyleBackColor = true;
+            this.btnwhile.Click += new System.EventHandler(this.btnwhile_Click);
+            // 
+            // btndowhile
+            // 
+            this.btndowhile.Location = new System.Drawing.Point(265, 310);
+            this.btndowhile.Name = "btndowhile";
+            this.btndowhile.Size = new System.Drawing.Size(75, 23);
+            this.btndowhile.TabIndex = 7;
+            this.btndowhile.Text = "do-while";
+            this.btndowhile.UseVisualStyleBackColor = true;
+            this.btndowhile.Click += new System.EventHandler(this.btndowhile_Click);
+            // 
+            // btnfor
+            // 
+            this.btnfor.Location = new System.Drawing.Point(380, 310);
+            this.btnfor.Name = "btnfor";
+            this.btnfor.Size = new System.Drawing.Size(75, 23);
+            this.btnfor.TabIndex = 8;
+            this.btnfor.Text = "for";
+            this.btnfor.UseVisualStyleBackColor = true;
+            this.btnfor.Click += new System.EventHandler(this.btnfor_Click);
+            // 
+            // btnlimpar
+            // 
+            this.btnlimpar.Location = new System.Drawing.Point(203, 364);
+            this.btnlimpar.Name = "btnlimpar";
+            this.btnlimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnlimpar.TabIndex = 9;
+            this.btnlimpar.Text = "limpar";
+            this.btnlimpar.UseVisualStyleBackColor = true;
+            this.btnlimpar.Click += new System.EventHandler(this.btnlimpar_Click);
+            // 
+            // btnvoltar
+            // 
+            this.btnvoltar.Location = new System.Drawing.Point(324, 364);
+            this.btnvoltar.Name = "btnvoltar";
+            this.btnvoltar.Size = new System.Drawing.Size(75, 23);
+            this.btnvoltar.TabIndex = 10;
+            this.btnvoltar.Text = "voltar";
+            this.btnvoltar.UseVisualStyleBackColor = true;
+            this.btnvoltar.Click += new System.EventHandler(this.btnvoltar_Click);
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnvoltar);
+            this.Controls.Add(this.btnlimpar);
+            this.Controls.Add(this.btnfor);
+            this.Controls.Add(this.btndowhile);
+            this.Controls.Add(this.btnwhile);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtvalor);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form2";
             this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -56,5 +167,15 @@
         #endregion
 
         private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtvalor;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnwhile;
+        private System.Windows.Forms.Button btndowhile;
+        private System.Windows.Forms.Button btnfor;
+        private System.Windows.Forms.Button btnlimpar;
+        private System.Windows.Forms.Button btnvoltar;
     }
 }
--- condicionalNiviaDois/Form2.cs
+++ condicionalNiviaDois/Form2.cs
@@ -16,5 +16,51 @@ namespace condicionalNiviaDois
         {
             InitializeComponent();
         }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnwhile_Click(object sender, EventArgs e)
+        {
+            double valor, i, j;
+
+            valor = Convert.ToDouble(txtvalor.Text);
+            i = 1;
+            j = 1;
+
+            while (i != 0)
+            {
+                i = valor - j;
+                
+                j++;
+            }
+        }
+
+        private void btndowhile_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnfor_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnlimpar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnvoltar_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
